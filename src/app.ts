57060f28d832c3506c86352b47ad2a2529209e81
rajengupta90021 <rajengupta@xetabots.com>
2025-11-16 14:23:14 +0530

    added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
+
 import { globalErrorHandler } from "./common/middleware/globalErrorHandler";
 import cookieParser from "cookie-parser";
+import customerRouter from "./customer/customerRouter";
+import couponRouter from "./coupon/couponRouter";
 
 const app = express();
 app.use(cookieParser());
@@ -9,6 +12,18 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello from order service service!" });
 });
+
+
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Hello from order service service!" });
+});
+
+app.use("/customer", customerRouter);
+app.use("/coupons", couponRouter);
 
 app.use(globalErrorHandler);
 
